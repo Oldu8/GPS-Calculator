71323920fd9e76499267c4e60034137fead3a57e
oldu8 <dudkoleg@gmail.com>
2022-10-15 18:38:17 -0400

    restyle

--- gps/js/function.js
+++ gps/js/function.js
@@ -6,22 +6,22 @@ const createForm = () => {
     createButton()
 }
 
-const createInputs =()=> {
-    for (let i=0; i < 2; i++) {
-    const digitName = document.createElement('h3');
-    digitName.innerText = `Enter your ${i+1}st coordiante`;
+const createInputs = () => {
+    for (let i = 0; i < 2; i++) {
+        const digitName = document.createElement('h3');
+        digitName.innerText = `Enter your ${i + 1}st coordiante`;
 
-    const coordinate = document.createElement('input');
-    coordinate.setAttribute('name', `coordinate${i+1}`)
-    coordinate.classList.add('input__digit')
+        const coordinate = document.createElement('input');
+        coordinate.setAttribute('name', `coordinate${i + 1}`)
+        coordinate.classList.add('input__digit')
 
-    const coordinatesForm = document.querySelector('.form__digit')
-    coordinatesForm.appendChild(digitName);
-    coordinatesForm.appendChild(coordinate)
+        const coordinatesForm = document.querySelector('.form__digit')
+        coordinatesForm.appendChild(digitName);
+        coordinatesForm.appendChild(coordinate)
     }
 }
 
-const createButton =()=> {
+const createButton = () => {
     const button = document.createElement('input');
     button.setAttribute('type', 'button');
     button.setAttribute('value', 'Calculate');
@@ -29,7 +29,7 @@ const createButton =()=> {
     blockForDigits.appendChild(button);
 }
 
-const getValues =() => {
+const getValues = () => {
     const coordinatesForm = document.querySelector('.form__digit');
     const coordinate1 = (coordinatesForm.coordinate1.value).split(', ');
     const coordinate2 = (coordinatesForm.coordinate2.value).split(', ');
@@ -43,14 +43,14 @@ const getValues =() => {
     }
 
     return {
-        x1:coordinate1[0],
-        y1:coordinate1[1],
-        x2:coordinate2[0],
-        y2:coordinate2[1] 
+        x1: coordinate1[0],
+        y1: coordinate1[1],
+        x2: coordinate2[0],
+        y2: coordinate2[1]
     }
 }
 
-function convertor (values) {
+function convertor(values) {
     const coordinates = {};
     for (const [key, value] of Object.entries(values)) {
         coordinates[key] = (value * Math.PI) / 180;
@@ -60,14 +60,13 @@ function convertor (values) {
 
 function getDistance(values) {
     const gpsInRadians = convertor(values);
-    console.log(JSON.stringify(values))
-    const distance = 
-    Math.acos(
-    Math.sin(gpsInRadians.x1) *
-    Math.sin(gpsInRadians.x2) + 
-    Math.cos(gpsInRadians.x1) * 
-    Math.cos(gpsInRadians.x2) * 
-    Math.cos(gpsInRadians.y1 - gpsInRadians.y2));
+    const distance =
+        Math.acos(
+            Math.sin(gpsInRadians.x1) *
+            Math.sin(gpsInRadians.x2) +
+            Math.cos(gpsInRadians.x1) *
+            Math.cos(gpsInRadians.x2) *
+            Math.cos(gpsInRadians.y1 - gpsInRadians.y2));
 
     result = r * distance;
     pushResult(result)
@@ -82,11 +81,11 @@ function pushResult() {
 function showMap() {
     (document.getElementById('map').classList.remove('hidden'));
     createMap(getValues());
- } 
+}
 
 // Google Api block
 
-function createMap(value){
+function createMap(value) {
     let options = {
         zoom: 2,
         center: {
@@ -96,19 +95,19 @@ function createMap(value){
     }
     let map = new google.maps.Map(document.getElementById('map'), options);
 
-    let marker1 = new google.maps.Marker ({
+    let marker1 = new google.maps.Marker({
         position: {
             lat: parseInt(value.x1),
             lng: parseInt(value.y1)
         },
-        map:map
+        map: map
     })
-    
-    let marker2 = new google.maps.Marker ({
+
+    let marker2 = new google.maps.Marker({
         position: {
             lat: parseInt(value.x2),
             lng: parseInt(value.y2)
         },
-        map:map
+        map: map
     })
 }
